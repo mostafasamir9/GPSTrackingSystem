41ed9cb165609a033cda2f90a53222624a6e2418
Hussein2022 <73998702+Hussein2022@users.noreply.github.com>
2021-06-05 06:42:41 +0200

    Add files via upload

---
+++ GPS_Calculations.c
@@ -0,0 +1,35 @@
+/************************************************************************
+* 
+* Module      : LCD
+*
+* File Nmae   : LCD_program.c
+*
+* Describtion : Source file for "GPS Module" containing the function definitions for GPS driver
+*
+* Authors     : Mohamed Ahmed - Hussein Mahmoud
+*
+* Version     : V01
+*
+* Date        : 5 June 2021
+*
+***************************************************************************/
+#include "GPS_Calculations.h"
+
+//---------------------------------------------------------//
+/************************************************************  
+*  Describtion: static (private) function used to find the +ve value of the passed argument
+**************************************************************/
+static uint32_t absolute(int32_t num){
+	return (num >= 0)? num: -1*num;
+}
+
+
+//---------------------------------------------------------//
+/***********************************************************  
+*  Describtion: Function used to calculate the distance covered by from the start(initial point) 
+**************************************************************/
+uint32_t GPS_CalculateDistance(int32_t initial_x, int32_t current_x, int32_t initial_y, int32_t current_y)
+{	
+	return absolute(current_y - initial_y) + absolute(current_x - initial_x);
+}
+
---
+++ main.c
@@ -0,0 +1,64 @@
+#include"stdint.h"
+#include"tm4c123gh6pm.h"
+void SystemInit(){}
+	
+#include"BIT_MATH.h"
+#include"STD_TYPES.h"	
+	
+#include"DIO_interface.h"
+#include"SYSTICK_interface.h"
+
+#include"LCD_interface.h"	
+	
+#include"GPS_Calculations.h"
+	
+int main()
+{
+	int32_t x_initial = 10, y_initial = -20;  /* iniitial values for the start point (x,y)  */
+	
+	
+/****************** This is used as a test for now till functions are ready **************************/
+	int32_t arr_x[] = {13,   18,  25,  37,  40,  42,  50,  93};
+	int32_t arr_y[] = {-22, -29, -33, -35, -35, -35, -38, -38};
+	uint32_t current = 0; /* Dummy variable for the test case array for GPS function */
+	uint32_t Distance;  /* Distance covered from initial point */
+	
+	
+	SYSTICK_init();
+	DIO_init(PORTF, GREEN, OUTPUT, NO_PUR);
+	LCD_init();
+	
+
+	
+	while(1)
+	{
+		
+		LCD_Print("Distance: ");
+		
+		Distance = GPS_CalculateDistance(x_initial, arr_x[current], y_initial, arr_y[current]);	
+		
+		LCD_PrintNumber(Distance);
+		
+		if(Distance>=100)
+		{
+		DIO_WritePin(PORTF, GREEN, HIGH);
+		}
+		
+		else
+		{
+			DIO_WritePin(PORTF, GREEN, LOW);
+		}
+		
+		SYSTICK_BusyWaitms(1500);
+		
+		LCD_sendCommand(CLEAR_SCREEN);
+		
+		if(current<=8)
+		{ 
+		current++;
+		}
+		
+}
+	
+}
+
