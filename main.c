a06468b0b277aa42b6bbc2411415a80c48dd35e4
MostafaAkrsh <43042153+MostafaAkrsh@users.noreply.github.com>
2021-06-07 14:59:58 +0200

    main with interrupt handlers

--- main.c
+++ main.c
@@ -1,15 +1,31 @@
-#include "tm4c123gh6pm.h"
-#include "PF_init.h"
+#include "PFInterruptInit.h"
 #include "LED.h"
+#include "tm4c123gh6pm.h"
+#include "Systick.h"
 
-int main()
-{
-	PF_init();
-	uint16_t distance = 100;
-	while(1)
-	{
-		uint16_t x = 50;
-		if ( x > distance )
-			LED_ON(0);
-	}
+void GPIOF_Handler();
+void SysTick_Handler(void);
+
+
+int main(){
+		PFInterruptInit();
+		Systick_init();
+		EnableInterrupts();
+		while(1);
 }
+
+uint8_t i = 0;
+
+void GPIOF_Handler(void){
+	GPIO_PORTF_ICR_R |= 0x11; // acknowledge flag
+	i = (i + 1) % 3;
+	LED_ON(i);
+	
+	Systick_wait_1s();
+}
+
+void SysTick_Handler(void)
+{
+		LED_OFF();
+}
+
