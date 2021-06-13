fb1bd9eda1ca8f64841f8c45b9f13776caef5acd
Mohamed Ahmed <50495610+MohamedAhmed412000@users.noreply.github.com>
2021-06-13 20:29:47 +0200

    Add Parsing function to GPS module

--- GPS_Calculations.h
+++ GPS_Calculations.h
@@ -8,9 +8,9 @@
 *
 * Authors     : Mohamed Ahmed - Hussein Mahmoud
 *
-* Version     : V01
+* Version     : V02
 *
-* Date        : 5 June 2021
+* Date        : 11 June 2021
 *
 ***************************************************************************/
 #include "stdint.h"
@@ -35,3 +35,18 @@ static uint32_t absolute(int32_t num);
 */
 uint32_t GPS_CalculateDistance(double long1, double lat1, double long2, double lat2);
 
+
+//-------------------------------------------------------------//
+/* Function Parameters : uint32_t Final Distance
+*                        
+*  Function Return     : void
+*/
+Position GPS_get_coordinates(void);
+
+
+//-------------------------------------------------------------//
+/* Function Parameters : array of char, uint8_t size
+*                        
+*  Function Return     : double
+*/
+double GPS_S_to_f(char *str, uint8_t size);
