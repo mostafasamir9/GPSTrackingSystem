f8f532988e28d8b9a6d49b685233d4e25ff7b8cc
mo1998 <mo.181298@gmail.com>
2021-06-13 03:42:14 +0200

    Added read port function
    Function to Read required Port data for specific port.

--- Src/DIO_program.c
+++ Src/DIO_program.c
@@ -26,3 +26,27 @@ void DIO_WritePin(uint8 PORT, uint8 PIN, uint8 WriteValue)
 					
 	}
 }
+
+
+/************************************************************************************/
+/*
+*  Describtion: Function to Read required Port value
+*/
+uint32 DIO_ReadPort(uint8 PORT)
+{
+	switch(PORT)
+	{
+		case PORTA: return GPIO_PORTA_DATA_R;
+					 
+		case PORTB:	return GPIO_PORTB_DATA_R;
+				   
+		case PORTC: return GPIO_PORTC_DATA_R;
+					    
+		case PORTD: return GPIO_PORTD_DATA_R;
+					
+		case PORTE: return GPIO_PORTE_DATA_R;
+				       
+		default: return GPIO_PORTF_DATA_R;
+					
+	}
+}
