dbcec11af58f8a5a802bcd917b544105d7078935
MostafaAkrsh <43042153+MostafaAkrsh@users.noreply.github.com>
2021-06-13 20:23:09 +0200

    Interrupt enable

---
+++ PFInterruptInit.h
@@ -0,0 +1,28 @@
+// NEW PART
+/************************************************************************
+* 
+* Module      : INTERRUPT
+*
+* File Nmae   : PFInterruptInit.h
+*
+* Describtion : Header file for "INTERRUPT Part" containing the functions of Interrupt
+*
+* Author : Hussein Mahmoud - Mohamed Ahmed - Mohamed Fathy - Mohamed Ali - Mostafa Mohsen - Mostafa Samir
+*
+* Version     : V01
+*
+* Date        : 11 June 2021
+*
+***************************************************************************/
+
+#ifndef INTERRUPT_H
+#define INTERRUPT_H
+
+#include "tm4c123gh6pm.h"
+
+
+void PFInterruptInit(void);
+void EnableInterrupts(void);
+
+#endif
+
